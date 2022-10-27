e73cf08fa1d5de0714b2145936cb76157464e900
JOSE CARLOS AVALOS PEREZ <jcavalosp@correo.url.edu.gt>
2022-10-27 14:55:38 -0600

    Proyecto terminado

--- Main.js
+++ Main.js
@@ -1,1 +1,32 @@
-console.log("hola");
+
+const dato = document.querySelector(".data");
+
+var contador = 0;
+
+function incrementar(){
+    contador++;
+    if(contador > 0){
+        dato.style.color = "green";
+    }
+    if(contador == 0){
+        dato.style.color = "black";
+    }
+    dato.innerHTML = contador;
+}
+
+function decrementar(){
+    contador--;
+    if(contador < 0){
+        dato.style.color = "red";
+    }
+    if(contador == 0){
+        dato.style.color = "black";
+    }
+    dato.innerHTML = contador;
+}
+
+function reinicio(){
+    contador = 0;
+    dato.style.color = "black";
+    dato.innerHTML = contador;
+}
